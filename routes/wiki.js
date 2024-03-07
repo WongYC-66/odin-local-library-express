1b0042678d9bd79fde79c707b8167848e803ce3e
WongYC-66 <yengchien@hotmail.com>
2024-03-07 18:01:54 +0800

    first commit

---
+++ routes/wiki.js
@@ -0,0 +1,65 @@
+// wiki.js - Wiki route module.
+
+// Import the module
+const asyncHandler = require("express-async-handler");
+
+
+const express = require("express");
+const router = express.Router();
+
+// Home page route.
+router.get("/", function (req, res) {
+  res.send("Wiki home page");
+});
+
+// About page route.
+router.get("/about", function (req, res) {
+  res.send("About this wiki");
+});
+
+module.exports = router;const Book = require("../models/book");
+const asyncHandler = require("express-async-handler");
+
+exports.index = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Site Home Page");
+});
+
+// Display list of all books.
+exports.book_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book list");
+});
+
+// Display detail page for a specific book.
+exports.book_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
+});
+
+// Display book create form on GET.
+exports.book_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book create GET");
+});
+
+// Handle book create on POST.
+exports.book_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book create POST");
+});
+
+// Display book delete form on GET.
+exports.book_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book delete GET");
+});
+
+// Handle book delete on POST.
+exports.book_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book delete POST");
+});
+
+// Display book update form on GET.
+exports.book_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book update GET");
+});
+
+// Handle book update on POST.
+exports.book_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Book update POST");
+});
